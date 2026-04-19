acdf09208712da7aa766ca175c73f8e97e9caaa1
sakshilambtile-dot <sakshilambtile@gmail.com>
2026-04-19 19:56:12 +0530

    fix category enum

--- models/listing.js
+++ models/listing.js
@@ -33,6 +33,21 @@ const listingSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: "User",
     },
+    category: {
+    type: String,
+    enum: [
+        "trending",
+        "rooms",
+        "mountains",
+        "castles",
+        "camping",
+        "farms",
+        "arctic",
+        "domes",
+        "boats",
+    ],
+    required: true,
+},
 });
 
 listingSchema.post("findOneAndDelete", async(listing) => {
